9f574899061bd68ba91b90dbdf720dc7f7190e3b
Mike Bentzen <mike.bentzen@rgtech.com.au>
2014-01-10 23:21:05 +1000

    Removed extra classes and more meaningful variable names.

--- ClientHeartbeatCSharpExample.OAuth2Lib/Pulses.cs
+++ ClientHeartbeatCSharpExample.OAuth2Lib/Pulses.cs
@@ -7,43 +7,14 @@
     using System.Runtime.Serialization;
     using System.Text;
 
-    public class QuestionResult
-    {
-        [JsonProperty("id")]
-        public int Id { get; set; }
-        [JsonProperty("change")]
-        public double Change { get; set; }
-        [JsonProperty("score")]
-        public double Score { get; set; }
-        [JsonProperty("name")]
-        public double Name { get; set; }
-    }
-
-    public class ChangeResult
-    {
-        [JsonProperty("change")]
-        public double Change { get; set; }
-        [JsonProperty("count")]
-        public int Count { get; set; }
-    }
-
-    public class HeartbeatResult
-    {
-        [JsonProperty("received")]
-        public int Received { get; set; }
-        [JsonProperty("opted_out")]
-        public int OptedOut { get; set; }
-        [JsonProperty("total")]
-        public int Total { get; set; }
-    }
 
 	public class PulseResult {
 
 		[JsonProperty("id")]
-		public long Id { get; set; }
+        public int Id { get; set; }
 
         [JsonProperty("account_id")]
-        public long AccountId { get; set; }
+        public int AccountId { get; set; }
 
         [JsonProperty("code")]
         public string Code { get; set; }
@@ -58,13 +29,13 @@
 		public double Question1 { get; set; }
 
 		[JsonProperty("question_2")]
-        public long Question2 { get; set; }
+        public double Question2 { get; set; }
 
         [JsonProperty("question_3")]
-        public long Question3 { get; set; }
+        public double Question3 { get; set; }
 
         [JsonProperty("question_4")]
-        public long Question4 { get; set; }
+        public double Question4 { get; set; }
 
 		[JsonProperty("overall")]
         public double Overall { get; set; }
@@ -77,8 +48,8 @@
 
             using (StreamReader reader = new StreamReader(jsonStream))
             {
-                var things = reader.ReadToEnd();
-                return JsonConvert.DeserializeObject<List<PulseResult>>(things);
+                var jsonResult = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<PulseResult>>(jsonResult);
             }
 
             
